519fded4c2d43ca437be3c0e420825e7b6f8efd9
fbdcv <2434897168@qq.com>
2022-07-25 10:13:34 +0800

    Update

---
+++ JavaSE/JAVA_数组和字符串/JavaArray/src/Test01.java
@@ -0,0 +1,35 @@
+public class Test01 {
+    public static void main(String[] args) {
+        //创建一维数组
+        //1.使用new创建数组，整数数组里的元素值为默认值0
+        int a[] = new int[5];
+        //2.使用{}创建数组，值可以自定义
+        int b[] = {1,2,3,4,5};
+
+        //遍历一维数组
+        System.out.println("遍历数组a");
+        for (int i = 0; i <a.length ; i++) {
+            System.out.print(a[i]+" ");
+        }
+        System.out.println();
+
+        System.out.println("遍历数组b");
+        for (int i = 0; i <b.length ; i++) {
+            System.out.print(b[i]+" ");
+        }
+        System.out.println();
+
+        //另一种遍历方式
+        System.out.println("使用另一种遍历方式");
+        System.out.println("遍历数组a");
+        for(int i:a) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+        System.out.println("遍历数组b");
+        for(int i:b) {
+            System.out.print(i + " ");
+        }
+
+    }
+}
--- README.md
+++ README.md
@@ -1,9 +1,9 @@
 # JavaStudy
 ## JavaSE
 
-- Java基础知识			[Java基础知识](/Java_基础知识/Java基础知识.md)
-- Java图形编程			[GUI.md](/JAVA_GUI/GUI.md)
-- Java网络编程			[Java网络编程.md ](/JAVA_网络编程/Java网络编程.md)
-- Java多线程			    [Java多线程.md](/JAVA_多线程/Java多线程.md)
-- Java注解和反射 		[注解和反射.md](/JAVA_注解和反射/注解和反射.md)
+- Java基础知识			[Java基础知识](/JavaSE/Java_基础知识/Java基础知识.md)
+- Java图形编程			[GUI.md](/JavaSE/JAVA_GUI/GUI.md)
+- Java网络编程			[Java网络编程.md ](/JavaSE/JAVA_网络编程/Java网络编程.md)
+- Java多线程			    [Java多线程.md](/JavaSE/JAVA_多线程/Java多线程.md)
+- Java注解和反射 		[注解和反射.md](/JavaSE/JAVA_注解和反射/注解和反射.md)
 
